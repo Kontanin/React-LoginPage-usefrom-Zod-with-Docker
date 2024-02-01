5b57d254a654dd3b62120b9093c54507368fd08c
Kontanin <boss12853@gmail.com>
2024-02-01 20:50:57 +0700

    add spna

--- src/page/loginpage.js
+++ src/page/loginpage.js
@@ -1,75 +1,68 @@
-import { Link,useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form"
-import doPostRequest from "../req/req";
-import {z } from "zod"
-import InputWithLabel from "../component/InputWithLabel";
-import { zodResolver } from "@hookform/resolvers/zod";
-function Login(){
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import doPostRequest from '../req/req';
+import { z } from 'zod';
+import InputWithLabel from '../component/InputWithLabel';
+import { zodResolver } from '@hookform/resolvers/zod';
+function Login() {
   const navigate = useNavigate();
-  const LoginSchema=z.object({
-    email:z.string().email() ,
-    password: z.string()
-  })
-
+  const LoginSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  });
 
   const {
     register,
     formState: { errors },
     handleSubmit,
-
-
   } = useForm({ resolver: zodResolver(LoginSchema) });
 
-
-
   const onhandle = async (data) => {
-    let result=await doPostRequest(data,'http://localhost:5000/login')
-    console.log(result,"result")
-    if(result){
+    let result = await doPostRequest(data, 'http://localhost:5000/login');
+    console.log(result, 'result');
+    if (result) {
       navigate('/home');
     }
   };
 
-  return(
-  <div className="h-screen flex items-center justify-center rounded-lg  ">
-    <div  className="bg-violet-950 rounded-lg   text-zinc-50">
-    <h1 className="">login</h1>
-      <div className="bg-stone-400 rounded-lg">
-        <form onSubmit={handleSubmit(onhandle)}>
-            <div className="bg-stone-50 border text-black">
-          
-              <InputWithLabel label={"email"} 
-              placeholder={"email"} 
-              error={errors?.email}
-              form={register("email")}  />
+  return (
+    <div className="h-screen flex items-center justify-center rounded-lg  ">
+      <div className="bg-violet-950 rounded-lg   text-zinc-50">
+        <h1 className="flex items-center justify-center">login</h1>
+        <div className="bg-stone-400 rounded-lg">
+          <form onSubmit={handleSubmit(onhandle)}>
+            <div className="flex flex-col bg-stone-50 border text-black">
+              <InputWithLabel
+                label={'email'}
+                placeholder={'email'}
+                error={errors?.email}
+                form={register('email')}
+              />
 
-
-              <InputWithLabel 
-              label={"password"} 
-              placeholder={"password"} 
-              error={errors?.password}
-              form={register("password")} />
-
+              <InputWithLabel
+                label={'password'}
+                placeholder={'password'}
+                error={errors?.password}
+                form={register('password')}
+              />
             </div>
 
             <div className="flex content-center justify-center  w-25 bg-stone-50 border text-zinc-50">
-              <button className="content-center w-32 bg-purple-500 rounded-lg " >Submit</button>
+              <button className="content-center w-32 bg-purple-500 rounded-lg ">
+                Submit
+              </button>
             </div>
-
-        </form>
+          </form>
+        </div>
+        <div className="flex content-center  justify-center">
+          <h6 className="mr-1">if you don't have account please </h6>
+          <Link to="/Register" className="flex justify-center underline">
+            Register
+          </Link>
+        </div>
       </div>
-      <div className="flex content-center  justify-center">
-        <h6 className="mr-1">if you don't have account please </h6>
-        <Link to="/Register" className="flex justify-center underline">Register</Link>
-      </div>
-
     </div>
-
-  </div>
-
-  )
-
+  );
 }
-
 
 export default Login;
--- src/page/registerpage.js
+++ src/page/registerpage.js
@@ -1,116 +1,111 @@
-import { Link,useNavigate } from "react-router-dom";
-import { useState  } from "react";
-import {useForm} from "react-hook-form"
-import doPostRequest from "../req/req";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {z } from "zod"
-import InputWithLabel from "../component/InputWithLabel";
-function Register(){
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import doPostRequest from '../req/req';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import InputWithLabel from '../component/InputWithLabel';
+function Register() {
   const navigate = useNavigate();
 
-const registerSchema=z.object({
-  email:z.string().email() ,
-  password: z.string(),
-  fristname:z.string(),
-  lastname:z.string(),
-  username: z.string(),
-  // confirmPassword: z.string(),
-})
-const {
-  register,
-  formState: { errors },
-  handleSubmit,
-} = useForm({ resolver: zodResolver(registerSchema) });
-
+  const registerSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+    fristname: z.string(),
+    lastname: z.string(),
+    username: z.string(),
+    // confirmPassword: z.string(),
+  });
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ resolver: zodResolver(registerSchema) });
 
   const onhandle = (e) => {
-    let payload=e
+    let payload = e;
 
-      console.log(payload)
-      let result =doPostRequest(payload,'http://localhost:5000/register')
-      if(result){
-        navigate('/');
-      }
-
+    console.log(payload);
+    let result = doPostRequest(payload, 'http://localhost:5000/register');
+    if (result) {
+      navigate('/');
+    }
   };
 
-
-
-  return( 
+  return (
     <div className="h-screen flex items-center justify-center rounded-lg ">
-      <div className="bg-violet-950 rounded-lg w-96  text-zinc-50 text-black">
-      <h1 className="flex items-center justify-center text-slate-200 ">Register</h1>
-        <form onSubmit={handleSubmit(onhandle)}>  
-        <div className="bg-stone-50 border text-black">
-          <div >
-<InputWithLabel 
-label={"fristname"}
-placeholder={"fristname"}
-error={errors?.fristname}
-form={register("fristname")}
+      <div className="bg-violet-950 rounded-lg   text-zinc-50 text-black">
+        <h1 className="flex items-center justify-center text-slate-200 ">
+          Register
+        </h1>
+        <form onSubmit={handleSubmit(onhandle)}>
+          <div className="bg-stone-50 border text-black">
+            <div>
+              <InputWithLabel
+                label={'fristname'}
+                placeholder={'fristname'}
+                error={errors?.fristname}
+                form={register('fristname')}
+              />
+            </div>
 
-/>
-          </div>
+            <div>
+              <InputWithLabel
+                label={'lastname'}
+                placeholder={'lastname'}
+                error={errors?.lastname}
+                form={register('lastname')}
+              />
+            </div>
 
-          <div >
-          <InputWithLabel 
-label={"lastname"}
-placeholder={"lastname"}
-error={errors?.lastname}
-form={register("lastname")}
+            <div>
+              <InputWithLabel
+                label={'username'}
+                placeholder={'username'}
+                error={errors?.username}
+                form={register('username')}
+              />
+            </div>
+            <div>
+              <InputWithLabel
+                label={'email'}
+                placeholder={'email'}
+                error={errors?.email}
+                form={register('email')}
+              />
+            </div>
 
-/>
-          </div>
-
-          <div >
-            <InputWithLabel 
-label={"username"}
-placeholder={"username"}
-error={errors?.username}
-form={register("username")}
-
-/>
-          </div>
-          <div >
-          <InputWithLabel 
-label={"email"}
-placeholder={"email"}
-error={errors?.email}
-form={register("email")}
-/>
-          </div>
-
-          <div>
-          {/* {/* <InputWithLabel 
+            <div>
+              {/* {/* <InputWithLabel 
 label={"confirmPassword"}
 placeholder={"confirmPassword"}
 error={errors?.confirmPassword}
 from={register("confirmPassword")}
 /> */}
-<InputWithLabel 
-label={"Password"}
-placeholder={"Password"}
-error={errors?.password}
-form={register("password")}
-/> 
+              <InputWithLabel
+                label={'Password'}
+                placeholder={'Password'}
+                error={errors?.password}
+                form={register('password')}
+              />
+            </div>
 
+            <div className="flex content-center justify-center  w-25 bg-stone-50 border text-zinc-50">
+              <button className="content-center w-32 bg-purple-500 rounded-lg">
+                Submit
+              </button>
+            </div>
           </div>
-          
-          <div  className="flex content-center justify-center  w-25 bg-stone-50 border text-zinc-50">
-            <button className="content-center w-32 bg-purple-500 rounded-lg">Submit</button>
-          </div>
-        </div>
         </form>
         <div className="flex content-center  justify-center  text-zinc-50">
           <h6 className="mr-1 ">if you already have account please </h6>
-          <Link to="/" className="underline">Login</Link>
+          <Link to="/" className="underline">
+            Login
+          </Link>
         </div>
-
       </div>
-  </div>
-  )
-
+    </div>
+  );
 }
-
 
 export default Register;
